70fd4d9a2af08f3828cfbcb9b2779011585be4a6
AndrewZ <90226362+Andrew-NoTag@users.noreply.github.com>
2025-03-19 17:08:34 -0400

    add menu page

--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -1,10 +1,153 @@
 import React from "react";
 import styles from "../styles/MenuPage.module.css";
+import sampleDish from "../assets/menu/sample_dish.jpg";
 
 function MenuPage() {
   return (
     <div className={styles.menuPage}>
-        <h1>Menu</h1>
+      <div className={styles.title}>Menu</div>
+      <div className={styles.intro}>Caan Paai</div>
+      {/* menu content */}
+      <div className={styles.menuContainer}>
+        {/* column 1 */}
+        <div className={styles.column}>
+          <div className={styles.menuTitle}>Appetizers</div>
+          <div style={{ marginTop: "2rem" }}></div>
+          <div className={styles.menuItem}>
+            <img
+              className={styles.itemImage}
+              src={sampleDish}
+              alt="sample dish"
+            />
+            <div>
+              <div className={styles.itemName}>Spring Rolls</div>
+              <div className={styles.itemDescription}>
+                This is a description of the dish.
+              </div>
+            </div>
+          </div>
+          <div className={styles.breakLine}></div>
+          <div className={styles.menuItem}>
+            <img
+              className={styles.itemImage}
+              src={sampleDish}
+              alt="sample dish"
+            />
+            <div>
+              <div className={styles.itemName}>Spring Rolls</div>
+              <div className={styles.itemDescription}>
+                This is a description of the dish.
+              </div>
+            </div>
+          </div>
+          <div className={styles.breakLine}></div>
+          <div className={styles.menuItem}>
+            <img
+              className={styles.itemImage}
+              src={sampleDish}
+              alt="sample dish"
+            />
+            <div>
+              <div className={styles.itemName}>Spring Rolls</div>
+              <div className={styles.itemDescription}>
+                This is a description of the dish.
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* column 2 */}
+        <div className={styles.column}>
+          <div className={styles.menuTitle}>Entree</div>
+          <div style={{ marginTop: "2rem" }}></div>
+          <div className={styles.menuItem}>
+            <img
+              className={styles.itemImage}
+              src={sampleDish}
+              alt="sample dish"
+            />
+            <div>
+              <div className={styles.itemName}>Spring Rolls</div>
+              <div className={styles.itemDescription}>
+                This is a description of the dish.
+              </div>
+            </div>
+          </div>
+          <div className={styles.breakLine}></div>
+          <div className={styles.menuItem}>
+            <img
+              className={styles.itemImage}
+              src={sampleDish}
+              alt="sample dish"
+            />
+            <div>
+              <div className={styles.itemName}>Spring Rolls</div>
+              <div className={styles.itemDescription}>
+                This is a description of the dish.
+              </div>
+            </div>
+          </div>
+          <div className={styles.breakLine}></div>
+          <div className={styles.menuItem}>
+            <img
+              className={styles.itemImage}
+              src={sampleDish}
+              alt="sample dish"
+            />
+            <div>
+              <div className={styles.itemName}>Spring Rolls</div>
+              <div className={styles.itemDescription}>
+                This is a description of the dish.
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* column 3 */}
+        <div className={styles.column}>
+          <div className={styles.menuTitle}>Desert</div>
+          <div style={{ marginTop: "2rem" }}></div>
+          <div className={styles.menuItem}>
+            <img
+              className={styles.itemImage}
+              src={sampleDish}
+              alt="sample dish"
+            />
+            <div>
+              <div className={styles.itemName}>Spring Rolls</div>
+              <div className={styles.itemDescription}>
+                This is a description of the dish.
+              </div>
+            </div>
+          </div>
+          <div className={styles.breakLine}></div>
+          <div className={styles.menuItem}>
+            <img
+              className={styles.itemImage}
+              src={sampleDish}
+              alt="sample dish"
+            />
+            <div>
+              <div className={styles.itemName}>Spring Rolls</div>
+              <div className={styles.itemDescription}>
+                This is a description of the dish.
+              </div>
+            </div>
+          </div>
+          <div className={styles.breakLine}></div>
+          <div className={styles.menuItem}>
+            <img
+              className={styles.itemImage}
+              src={sampleDish}
+              alt="sample dish"
+            />
+            <div>
+              <div className={styles.itemName}>Spring Rolls</div>
+              <div className={styles.itemDescription}>
+                This is a description of the dish.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
